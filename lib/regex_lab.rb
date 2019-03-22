723b8e7cf0932854fd90098beeee627e16af0789
James Cox <jamesncox@yahoo.com>
2019-03-22 16:09:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,11 +10,14 @@ def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
 end
 
+# def first_word_capitalized_and_ends_with_punctuation?(text)
+#   text.match(/^[A-Z].+[\.!?]$/) ? true : false
+# end
+
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].+[\.!?]$/) ? true : false
+ text.match(/^[A-Z].+[:punct:]$/) ? true : false
+
 end
-
-
 
 def valid_phone_number?(phone)
   phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
