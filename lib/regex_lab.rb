a28fec2f1d9ac52eaad935dd84a05249930462a9
James Cox <jamesncox@yahoo.com>
2019-03-22 16:43:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,3 +24,7 @@ end
 #   phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) ? true : false
 # end
 
+def valid_phone_number?(phone)
+  phone.scan(/^\D{3}\D(0,1)\D{3}\D(0,1)\D{4}$/) ? true : false
+  binding.pry
+end
